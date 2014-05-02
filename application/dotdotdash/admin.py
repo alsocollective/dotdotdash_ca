37af78bfb8cd0c5604a5ccbe20465c4282a7e40b
Bohdan <bohdan.anderson@gmail.com>
2014-05-02 14:40:55 -0400

    is there anything

--- application/dotdotdash/admin.py
+++ application/dotdotdash/admin.py
@@ -1,7 +1,87 @@
 from django.contrib import admin
 from dotdotdash.models import *
 
+class mediaAdmin(admin.ModelAdmin):
+	list_display = ('title','admin_image')
+	readonly_fields = ["admin_image",]
+	search_fields = ('title', )
+	fieldsets = [
+		(None,{'fields':[('location',"admin_image")]}),
+		('Advance options', {
+			'fields':('link','order','vimeo','description'),
+			}),
+	]
+
+class HomeAdmin(admin.ModelAdmin):
+	readonly_fields=['bk_image',]
+	fieldsets = [
+		("Quotes",{'fields':['quote1','quote2',"quote3","quote4"]}),
+		("Video",{'fields':[('workbackgroundimage','bk_image'),'youtubeId']})
+	]
+
+class ClientsAdmin(admin.ModelAdmin):
+	filter_horizontal = ("clientimages",)
+	readonly_fields = ["allClients",]
+	fieldsets= [
+		(None,{'fields':['text','clientimages','allClients']})
+	]
+
+class ContactAdmin(admin.ModelAdmin):
+	readonly_fields=['admin_image',]
+	fieldsets = [(None,{'fields':['contant',('backgroundimage','admin_image')]})]
+
+class AboutAdmin(admin.ModelAdmin):
+	readonly_fields= ["admin_image",]
+	fieldsets= [
+		(None,{'fields':['about','process',('backgroundimage','admin_image')]})
+	]
+
+class ServicesAdmin(admin.ModelAdmin):
+	readonly_fields = [
+		"bk_image",
+		"soc_image",
+		"brandingimage_image",
+		"researchimage_image",
+		"creativedevimage_image",
+		"contentproductionimage_image",
+		"resultsimage_image",
+	]
+	fieldsets = [
+		(None,{	'fields':
+			[
+				"services",
+				"socialmediastrategyplanning",
+				"branding",
+				"research",
+				"creativedevelopment",
+				"contentproduction",
+				"resultsanalytics",
+				"backgroundimage",
+				"bk_image",
+				"socimage",
+				"soc_image",
+				"brandingimage",
+				"brandingimage_image",
+				"researchimage",
+				"researchimage_image",
+				"creativedevimage",
+				"creativedevimage_image",
+				"contentproductionimage",
+				"contentproductionimage_image",
+				"resultsimage",
+				"resultsimage_image",
+			]})
+	]
+
+admin.site.register(Home,HomeAdmin)
+admin.site.register(About,AboutAdmin)
+admin.site.register(Services,ServicesAdmin)
+admin.site.register(Clients,ClientsAdmin)
+admin.site.register(Contact,ContactAdmin)
+
 class pageAdmin(admin.ModelAdmin):
+	def get_model_perms(self, request):
+		return {}
 	list_display = ('title','project','order','pageType')
 	filter_horizontal = ("mediaField",)
 	list_filter = ('project', )
@@ -13,7 +93,6 @@ class pageAdmin(admin.ModelAdmin):
 			'description':("Add the Viemo url(from the embeded method) here it should look something like this... http://player.vimeo.com/video/52542094?title=0&amp;byline=0&amp;portrait=0&amp;color=ff0179"),
 			'fields':['videoURL'],
 			}),
-
 		("PDF",{
 			'description':("Upload the pdf here, and a jpg of the PDF in the mediaField"),
 			'fields':['pdf'],
@@ -22,65 +101,84 @@ class pageAdmin(admin.ModelAdmin):
 			'description':("This text will be displayed when the proper pageType"),
 			'fields':['textFields'],
 			}),
-
 	]
 
-class mediaAdmin(admin.ModelAdmin):
-	list_display = ('title','admin_image')
+class workInline(admin.TabularInline):
+	model = Work.pages.through
+	extra = 0
+	readonly_fields = ['changeform_link','orderObject',]
+	def changeform_link(self,instance):
+		if instance.id:
+			return u'<a href="/admin/dotdotdash/page/%s/" target="_blank">Edit</a>' % (instance.page_id)
+		return u'The link is not available till after we save'
+	changeform_link.allow_tags = True
+	changeform_link.short_description = 'Link'
 
+	def orderObject(self,instance):
+		if instance.page_id:
+			curPage = Page.objects.get(id=instance.page_id)
+			if(not curPage):
+				return "not object found"
+			return curPage.order
+	orderObject.allow_tags = True
+	orderObject.short_description="Order"
 
-	fieldsets = [
-		(None,{'fields':['location','link']}),
-		('Advance options', {
-			'classes':('collapse',),
-			'fields':('description','title','order','vimeo'),
-			}),
-	]
-
-
-
-class HomeAdmin(admin.ModelAdmin):
-	fieldsets = [
-		("Quotes",{'fields':['quote1','quote2',"quote3","quote4"]}),
-		("Video",{'fields':['workbackgroundimage','youtubeId']})
-	]
-
-class ClientsAdmin(admin.ModelAdmin):
-	filter_horizontal = ("clientimages",)
-
-
-
-
-
-
+	verbose_name = "Page"
+	verbose_name_plural = "Pages"
 
 
 class workAdmin(admin.ModelAdmin):
-	list_display = ('title','order',"is_a_sos_project")
-	filter_horizontal = ("pages","image")
-	fieldsets = [(None,{'fields':[("title","subTitle","order"),"pages","image"]})]
-
-class ProjectAdmin(admin.ModelAdmin):
-	filter_horizontal = ("clientimages",)
 	list_display = ('title','order')
-	fieldsets = [
-		(None,{'fields':[('title','order'),'clientimages','slug']})
-	]
-
-class perPage(admin.ModelAdmin):
-	filter_horizontal = ("projects",)
-	list_display = ('title','slug')
+	filter_horizontal = ("image",)
+	inlines = [ workInline, ]
+	fieldsets = [(None,{'fields':[("title","subTitle","order"),"image"]})]
 
 
 
-admin.site.register(Home,HomeAdmin)
-admin.site.register(About)
-admin.site.register(Services)
-admin.site.register(Clients,ClientsAdmin)
+
 admin.site.register(MediaNode,mediaAdmin)
 admin.site.register(Work,workAdmin)
 admin.site.register(Page,pageAdmin)
-admin.site.register(Contact)
+
+
+
+
+
+class ProjectAdmin(admin.ModelAdmin):
+	def get_model_perms(self, request):
+		return {}
+	filter_horizontal = ("clientimages",)
+	list_display = ('title','order')
+	fieldsets = [(None,{'fields':[('title','order'),'clientimages','slug']})]
+
+class ProjectsInline(admin.TabularInline):
+	model = PersonalizedPage.projects.through
+	extra = 0
+	readonly_fields = ['changeform_link', 'orderObject',]
+	def changeform_link(self,instance):
+		if instance.project_id:
+			return u'<a href="/admin/dotdotdash/project/%s/" target="_blank">Edit</a>' % instance.project_id
+		return u'The link is not available till after we save'
+	changeform_link.allow_tags = True
+	changeform_link.short_description = 'Link'
+
+	def orderObject(self,instance):
+		if instance.project_id:
+			curPage = Project.objects.get(id=instance.project_id)
+			if(not curPage):
+				return "not object found"
+			return curPage.order
+	orderObject.allow_tags = True
+	orderObject.short_description="Order"
+
+	verbose_name = "Project"
+
+class PersonalizedPageInlineAdmin(admin.ModelAdmin):
+	model = PersonalizedPage
+	inlines = [ ProjectsInline, ]
+	fieldsets = [(None,{'fields':[("title","slug",),"textBlock"]})]
+	readonly_fields = ["slug"]
 
 admin.site.register(Project,ProjectAdmin)
-admin.site.register(PersonalizedPage,perPage)
+admin.site.register(PersonalizedPage,PersonalizedPageInlineAdmin)
+
--- application/dotdotdash/models.py
+++ application/dotdotdash/models.py
@@ -9,9 +9,9 @@ import os.path
 class MediaNode(models.Model):
 	title = models.CharField(max_length=600,blank=True)
 	description = models.CharField(max_length=300, blank=True)
-	link = models.URLField(max_length=1000, blank=True);
-	vimeo = models.CharField(max_length=1000, blank=True);
-	order = models.IntegerField(blank=True,default=0)
+	link = models.URLField(max_length=1000, blank=True,help_text="For links to external sites; used for 'Client Images', and links to websites");
+	vimeo = models.CharField(max_length=1000, blank=True,help_text="This is only used for Presentation Pages (submitting for competition links), the link should look like 'http://vimeo.com/85560958' . Be sure to also add a image, it will be show before playing the video. ");
+	order = models.IntegerField(blank=True,default=99)
 
 	def slugify_filename(instance, filename):
 		fname, dot, extension = filename.rpartition('.')
@@ -20,7 +20,7 @@ class MediaNode(models.Model):
 		return '%s.%s' % (slug, extension)
 
 	location = models.FileField(upload_to=slugify_filename)
-
+	location.verbose_name=u'Uploaded element'
 	def save(self, *args, **kwargs):
 		fname, dot, extension = os.path.basename(self.location.name).rpartition('.')
 		fname = slugify(fname)
@@ -55,9 +55,17 @@ class Home(models.Model):
 	ogv = models.ForeignKey(MediaNode,blank=True,null=True,related_name="ogv+")
 	webm = models.ForeignKey(MediaNode,blank=True,null=True,related_name="webm+")
 	workbackgroundimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="bkimg+")
-	youtubeId = models.CharField(max_length=100,blank=True)
+	youtubeId = models.CharField(max_length=100,blank=True,help_text='Should look something like "1PxQj_HiQjo"')
 	quote3 = models.TextField(max_length=1000)
 	quote4 = models.TextField(max_length=1000)
+
+	def bk_image(self):
+		if self.workbackgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.workbackgroundimage
+		return "not an image"
+	bk_image.allow_tags = True
+	bk_image.short_description = "Selected Image"
+
 
 class About(models.Model):
 	class Meta:
@@ -65,6 +73,13 @@ class About(models.Model):
 	about =	models.TextField(max_length=4000)
 	process = models.TextField(max_length=4000)
 	backgroundimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="bkimg+")
+
+	def admin_image(self):
+		if self.backgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.backgroundimage
+		return "not an image"
+	admin_image.allow_tags = True
+	admin_image.short_description = "Selected Image"
 
 class Services(models.Model):
 	class Meta:
@@ -80,12 +95,63 @@ class Services(models.Model):
 	resultsanalytics = models.TextField(max_length=2000)
 
 	backgroundimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="bkimg+")
+	backgroundimage.short_description = 'Social media'
+	backgroundimage.verbose_name=u'Social media'
+	def bk_image(self):
+		if self.backgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.backgroundimage
+		return "not an image"
+	bk_image.allow_tags = True
+	bk_image.short_description = "Selected Image"
+
 	socimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="1img+")
+	def soc_image(self):
+		if self.backgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.socimage
+		return "not an image"
+	soc_image.allow_tags = True
+	soc_image.short_description = "Selected Image"
+
 	brandingimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="2img+")
+	def brandingimage_image(self):
+		if self.backgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.brandingimage
+		return "not an image"
+	brandingimage_image.allow_tags = True
+	brandingimage_image.short_description = "Selected Image"
+
 	researchimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="3img+")
+	def researchimage_image(self):
+		if self.backgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.researchimage
+		return "not an image"
+	researchimage_image.allow_tags = True
+	researchimage_image.short_description = "Selected Image"
+
 	creativedevimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="4img+")
+	def creativedevimage_image(self):
+		if self.backgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.creativedevimage
+		return "not an image"
+	creativedevimage_image.allow_tags = True
+	creativedevimage_image.short_description = "Selected Image"
+
 	contentproductionimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="5img+")
+	def contentproductionimage_image(self):
+		if self.backgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.contentproductionimage
+		return "not an image"
+	contentproductionimage_image.allow_tags = True
+	contentproductionimage_image.short_description = "Selected Image"
+
 	resultsimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="6img+")
+	def resultsimage_image(self):
+		if self.backgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.resultsimage
+		return "not an image"
+	resultsimage_image.allow_tags = True
+	resultsimage_image.short_description = "Selected Image"
+
 
 class Clients(models.Model):
 	class Meta:
@@ -93,6 +159,23 @@ class Clients(models.Model):
 	text = models.TextField(max_length=2000)
 	clientimages = models.ManyToManyField(MediaNode,blank=True,related_name="imageFields+")
 	backgroundimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="bkimg+")
+
+	def bk_image(self):
+		if self.backgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.backgroundimage
+		return "not an image"
+	bk_image.allow_tags = True
+	bk_image.short_description = "Selected Image"
+
+	def allClients(self):
+		out = "<div style='text-align:center;'><br>"
+		if self.clientimages:
+			for img in self.clientimages.order_by("order").all():
+				out += '<a href="/admin/dotdotdash/medianode/%s/" target="_blank" style="text-align:left;display:inline-block; padding:1em; border:1px solid #eee"><label style="float:none;width:auto;font-weight: bold !important;color: #333 !important;">title: %s<br>link: %s<br>order: %s</label> <img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/><br></a>' % (img.id,img,img.link,img.order,img)
+		out += "</div>"
+		return out
+	allClients.allow_tags = True
+	allClients.short_description = "Selected Clients"
 
 class Page(models.Model):
 	class Meta:
@@ -112,7 +195,7 @@ class Page(models.Model):
 	videoURL = models.URLField(max_length=800, blank=True)
 	pageType = models.CharField(max_length=30, choices=pageTypes)
 	slug = models.SlugField(blank=True)
-	order = models.IntegerField(blank=True,default=1)
+	order = models.IntegerField(blank=True,default=99)
 	pdf = models.ManyToManyField(MediaNode,blank=True,related_name="pdf+")
 	project = models.CharField(max_length=400,blank=True,default="not defined")
 	def save(self,*args, **kwargs):
@@ -131,8 +214,8 @@ class Work(models.Model):
 	description = models.TextField(max_length=1000)
 	datefororder = models.DateField(auto_now=True)
 	slug = models.SlugField(blank=True)
-	order = models.IntegerField(blank=True,default=1)
-	is_a_sos_project = models.BooleanField(blank=True)
+	order = models.IntegerField(blank=True,default=99)
+	is_a_sos_project = models.BooleanField(blank=True,default=False)
 	image = models.ManyToManyField(MediaNode,blank=True)
 
 	def save(self,*args, **kwargs):
@@ -147,7 +230,12 @@ class Contact(models.Model):
 		verbose_name_plural = "Contact"
 	contant = models.TextField(max_length=4000)
 	backgroundimage = models.ForeignKey(MediaNode,blank=True,null=True,related_name="bkimg+")
-
+	def admin_image(self):
+		if self.backgroundimage:
+			return '<img style="width:200px;height:auto;" src="http://also-static.com/dotdotdash/uploaded/%s"/>' % self.backgroundimage
+		return "not an image"
+	admin_image.allow_tags = True
+	admin_image.short_description=u'BackgroundImage'
 
 class Project(models.Model):
 	class Meta:
@@ -156,6 +244,7 @@ class Project(models.Model):
 	slug = models.SlugField(blank=True)
 	clientimages = models.ManyToManyField(MediaNode)
 	order = models.IntegerField(default=99, blank=True)
+	clientimages.verbose_name = "Included media"
 
 	def save(self,*args, **kwargs):
 		self.slug = slugify(self.title)
@@ -168,7 +257,7 @@ class PersonalizedPage(models.Model):
 	class Meta:
 		verbose_name = 'Presentation Page'
 	title = models.CharField(max_length=600)
-	slug = models.SlugField(blank=True)
+	slug = models.SlugField(blank=True,verbose_name="link")
 	textBlock = models.TextField(max_length=4000, blank=True)
 	projects = models.ManyToManyField(Project)
 
